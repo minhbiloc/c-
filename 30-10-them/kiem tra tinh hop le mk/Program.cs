d6fd3ee6160cdfa10c92d25f65dbba22ffa99bce
minhbiloc <nqmminh2k3@gmail.com>
2024-10-30 22:39:40 +0700

    bai tap lam them

---
+++ 30-10-them/kiem tra tinh hop le mk/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiem_tra_tinh_hop_le_mk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            Console.WriteLine("mời nhập vào mk( ít nhất 6 ký tự, ít nhất 1 chữ cái, ít nhất 1 chữ số): ");
+            string mk = Console.ReadLine(); //123456a
+            bool check = true;
+            int demSo = 0, demKytu = 0;
+
+            while (check == true)
+            {
+                foreach (char c in mk)
+                {
+                    if (char.IsDigit(c))
+                        demSo++;
+                    else if (char.IsLetter(c))
+                        demKytu++;
+                }
+
+                Console.WriteLine(demSo);
+                Console.WriteLine(demKytu);
+
+                if (demKytu * demSo != 0 && mk.Length >= 6)
+                {
+                    check = false;
+                }
+                else
+                {
+                    Console.WriteLine("nhập lại mk ,( ít nhất 6 ký tự, ít nhất 1 chữ cái, ít nhất 1 chữ số)");
+                    mk = Console.ReadLine();
+                    check = true;
+                }
+            }
+            Console.WriteLine("bạn đã thiết lập mật khẩu thành công , mk của bạn là " + mk);
+            string login;
+            int demLogin = 0;   
+
+            Console.WriteLine("mời nhập vào mật khẩu đăng nhập: ");
+            login = Console.ReadLine();
+            while (true)
+            {
+                bool kq = mk.Equals(login);
+                if (kq)
+                {
+                    Console.WriteLine("đăng nhập thành công");
+                    break;
+
+                }
+                else
+                {
+                    demLogin++;
+                    if (demLogin < 5)
+                    {
+                        Console.WriteLine($" nhập lại mk , nhập mật khẩu sai {demLogin}/5 lần");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" bạn đã đăng nhập sai 5 lần , khóa đăng nhập");
+                        break ;
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 30-10-them/viết chương trình chuyển tin nhắn sang mật mã bảng/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viết_chương_trình_chuyển_tin_nhắn_sang_mật_mã_bảng
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string a = "adkhbfjhfcsd";
+            string b = "dkjfsjgcfkbjgsfs";
+            Console.WriteLine(" mời nhập vào chuỗi cần mã hóa");
+            string chuoiInput = Console.ReadLine();//abc
+            string chuoiOutput = "";
+            foreach (char c in chuoiInput)
+            {
+                Console.WriteLine($"{c}");
+                int indexKyTu = a.IndexOf(c);
+                Console.WriteLine(b[indexKyTu]);
+                chuoiOutput += b[indexKyTu];
+            }
+            Console.WriteLine(chuoiOutput);
+         Console.ReadLine();
+        }
+    }
+}
